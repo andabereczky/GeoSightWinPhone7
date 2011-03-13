c7501b16bee5b30af0fe10a9c2bb4ed4ff68c63c
Anda Bereczky <anda@bereczky.me>
2011-03-13 15:34:50 -0500

    Added GPS capabilities and come tests.

---
+++ GeoSight.Tests/GPSLocationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeoSight.Tests
+{
+    [TestClass]
+    public class GPSLocationTest
+    {
+        [TestMethod]
+        public void TestGPSLocation()
+        {
+            GPSLocation location = new GPSLocation();
+            Assert.IsTrue(location.Started);
+        }
+    }
+}
---
+++ GeoSight.Tests/WebClientTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeoSight.Tests
+{
+    [TestClass]
+    public class WebClientTest
+    {
+        [TestMethod]
+        public void TestSendRequest()
+        {
+            new WebClient().SendReqest(
+                true,
+                "http://www.google.com/",
+                new Dictionary<String, String>(),
+                new EventDelegates.HTTPResponseDelegate(ProcessResponse),
+                new EventDelegates.HTTPFailDelegate(FailRequest));
+        }
+
+        private void ProcessResponse(Stream responseStream)
+        {
+            StreamReader reader = new StreamReader(responseStream);
+            string response = reader.ReadToEnd();
+            Debug.WriteLine(response);
+            Assert.IsTrue(response.Length > 0);
+            reader.Close();
+        }
+
+        private void FailRequest(String message)
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
