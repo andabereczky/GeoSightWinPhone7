40bc26088cc4b19ab9975e2abcc95dba87941655
Anda Bereczky <anda@bereczky.me>
2011-05-01 22:28:36 -0500

    Commented PickSightPage

--- GeoSight/PickSightPage.xaml.cs
+++ GeoSight/PickSightPage.xaml.cs
@@ -1,17 +1,26 @@
-﻿using Microsoft.Phone.Controls;
-using System;
+﻿using System;
+using System.Windows;
 using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
 
 namespace GeoSight
 {
     public partial class PickSightPage : PhoneApplicationPage
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public PickSightPage()
         {
+            // Initialize the page
             InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        /// <summary>
+        /// Called when a page becomes the active page in a frame.
+        /// </summary>
+        /// <param name="eventArgs">The event arguments.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs eventArgs)
         {
             // Check if a sight has been selected.
             if (App.SelectedSight != null)
@@ -21,12 +30,22 @@ namespace GeoSight
             }
         }
 
-        private void btn_FromList_Click(object sender, System.Windows.RoutedEventArgs e)
+        /// <summary>
+        /// Called when the "From a list" button is clicked.
+        /// </summary>
+        /// <param name="sender">The notifying object.</param>
+        /// <param name="eventArgs">The event arguments.</param>
+        private void ShowPickSightFromListPage(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new Uri("/PickSightListPage.xaml", UriKind.Relative));
         }
 
-        private void btn_FromMap_Click(object sender, System.Windows.RoutedEventArgs e)
+        /// <summary>
+        /// Called when the "From a map" button is clicked.
+        /// </summary>
+        /// <param name="sender">The notifying object.</param>
+        /// <param name="eventArgs">The event arguments.</param>
+        private void ShowPickSightFromMapPage(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new Uri("/PickSightMapPage.xaml", UriKind.Relative));
         }
